8fecd2ebf959fa4a82dcc54acfcc3770822dfea8
Eli T <eliyhaoo@gmail.com>
2022-04-04 17:11:33 +0300

    added RenderProjects

--- js/main.js
+++ js/main.js
@@ -1,28 +1,37 @@
-console.log('Starting up');
-
-function init() {
+'use strict'
+$(onInit)
+function onInit() {
     renderProjects()
 }
 
-function renderPrjoects() {
-
-
+function renderProjects() {
+   var projects =  getGprjoects()
+   console.log('prjoects',projects);
+  const projsSTR= projects.map(project => {
+     return `
+   <div class="col-md-4 col-sm-6 portfolio-item">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal">
+            <div class="portfolio-hover">
+              <div class="portfolio-hover-content">
+                <i class="fa fa-plus fa-3x"></i>
+              </div>
+            </div>
+            <img class="img-fluid" src="${project.url}" alt="">
+          </a>
+          <div class="portfolio-caption">
+            <h4>${project.title}</h4>
+            <p class="text-muted">${project.labels[0]}</p>
+          </div>
+        </div>
+   ` 
+})
+$('.projects-container').html(projsSTR)
 }
 
-var gProjects = [
-    {
-        id: "touchNums",
-        name: "touchNums",
-        title: "Touch The Numbers",
-        desc: "In this project you touch the numbers", url: "projs/sokoban",
-        publishedAt: 1448693940000,
-        labels: ["Matrixes", "keyboard events"],
-    }
-]
 
 
-var newDate = new Date (2022,02,17).getTime()
-// var newDatet = newDate.getTime()
+// var newDate = new Date (2022,02,17).getTime()
+// // var newDatet = newDate.getTime()
 
-console.log(new Date(newDate));
-// console.log('read',newDatet);
+// console.log(new Date(newDate));
+// // console.log('read',newDatet);
--- js/service-prjoects.js
+++ js/service-prjoects.js
@@ -1,1 +1,59 @@
 'use strict'
+
+
+
+
+function getGprjoects(){
+    return gProjects
+}
+
+var gProjects = [
+    {
+        id: "touchNums",
+        name: "touchNums",
+        title: "Touch The Numbers",
+        desc: "In this project you touch the numbers", url: "projs/sokoban",
+        publishedAt: new Date(2022,2,17).getTime(),
+        labels: ["game", "mouse"],
+    },
+    {
+        id: "chess",
+        name: "chess",
+        title: "Chess",
+        desc: "Do you need me to teach you chess?", url: "projs/sokoban",
+        publishedAt: new Date (2022,2,18).getTime(),
+        labels: ["game", "keyboard events"],
+    },
+    {
+        id: "packman",
+        name: "packman",
+        title: "Packman",
+        desc: "well.. eat them all!!! , look for the extra candies", url: "projs/sokoban",
+        publishedAt: new Date (2022,2,19).getTime(),
+        labels: ["game", "keyboard events"],
+    },
+    {
+        id: "minesSweeper",
+        name: "minesSweeper",
+        title: "Mines Sweeper!",
+        desc: "Choose your level and try not to explode!, try to beat the High score! ", url: "projs/sokoban",
+        publishedAt: new Date (2022,2,20).getTime(),
+        labels: ["game", "keyboard events"],
+    },
+    {
+        id: "waiting to fill",
+        name: "touchNums",
+        title: "Touch The Numbers",
+        desc: "In this project you touch the numbers", url: "projs/sokoban",
+        publishedAt: new Date (2022,2,17).getTime(),
+        labels: ["game", "keyboard events"],
+    },
+    {
+        id: "waiting to fill",
+        name: "touchNums",
+        title: "Touch The Numbers",
+        desc: "In this project you touch the numbers", url: "projs/sokoban",
+        publishedAt: new Date (2022,2,17).getTime(),
+        labels: ["game", "keyboard events"],
+    },
+]
