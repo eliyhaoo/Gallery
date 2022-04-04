f62272a0f311c42dcae8662266cf354daf1972e5
Eli T <eliyhaoo@gmail.com>
2022-04-04 21:13:16 +0300

    finished Contact me + Modal

--- js/main.js
+++ js/main.js
@@ -10,13 +10,13 @@ function renderProjects() {
   const projsSTR= projects.map(project => {
      return `
    <div class="col-md-4 col-sm-6 portfolio-item">
-          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal">
+          <a id="${project.id}"class="portfolio-link" data-toggle="modal" onclick="onClickedProject(this.id)" href="#portfolioModal1">
             <div class="portfolio-hover">
               <div class="portfolio-hover-content">
                 <i class="fa fa-plus fa-3x"></i>
               </div>
             </div>
-            <img class="img-fluid" src="${project.url}" alt="">
+            <img class="img-fluid" src="img/portfolio/${project.id}.png" alt="">
           </a>
           <div class="portfolio-caption">
             <h4>${project.title}</h4>
@@ -28,10 +28,44 @@ function renderProjects() {
 $('.projects-container').html(projsSTR)
 }
 
+function onClickedProject(projId){
+  var projects = getGprjoects()
+  var project = projects.find(proj =>  proj.id === projId)
+  updateModal(project)
+
+}
+
+function updateModal(project){
+  console.log('project in update', project);
+  console.log('project in update',project);
+  $('.modal-body h2').text(project.title)
+  console.log(project.desc);
+  $('.modal-body p.item-intro').text(project.desc)
+  $('.modal-body img').attr('src',`img/portfolio/${project.id}.png`)
+  $('.modal-body .date').text(_getDate(project.publishedAt))
+  $('.modal-body .check-out').attr('href',project.url)
+  console.log(project.publishedAt);
+  
+}
+
+function onSubmitForm(){
+  // ev.preventDefault()
+  var email = $('.contact input[id=defaultContactFormEmail]').val()
+  var subject = $('.contact input[id=defaultContactFormName]').val()
+  var message = $('.contact textarea[id=exampleFormControlTextarea2]').val()
+ 
+  window.open(`https://mail.google.com/mail/?view=cm&fs=1&to=eliyhaoo@gmail.com&su=${subject}&body=${email+'  '+message}`)
+  
+  email.text('')
+  subject.text('')
+  message.text('')
+
+}
+
+function openEmailWindow() {
+}
 
 
-// var newDate = new Date (2022,02,17).getTime()
-// // var newDatet = newDate.getTime()
-
-// console.log(new Date(newDate));
-// // console.log('read',newDatet);
+function _getDate(date){
+  return `${date.getDate()}-${date.getMonth()}-2022`// .getYear doesn't work - should I use a diff method?
+}
--- js/service-prjoects.js
+++ js/service-prjoects.js
@@ -13,7 +13,7 @@ var gProjects = [
         name: "touchNums",
         title: "Touch The Numbers",
         desc: "In this project you touch the numbers", url: "projs/sokoban",
-        publishedAt: new Date(2022,2,17).getTime(),
+        publishedAt: new Date(2022,2,17),
         labels: ["game", "mouse"],
     },
     {
@@ -21,7 +21,7 @@ var gProjects = [
         name: "chess",
         title: "Chess",
         desc: "Do you need me to teach you chess?", url: "projs/sokoban",
-        publishedAt: new Date (2022,2,18).getTime(),
+        publishedAt: new Date (2022,2,18),
         labels: ["game", "keyboard events"],
     },
     {
@@ -29,15 +29,15 @@ var gProjects = [
         name: "packman",
         title: "Packman",
         desc: "well.. eat them all!!! , look for the extra candies", url: "projs/sokoban",
-        publishedAt: new Date (2022,2,19).getTime(),
+        publishedAt: new Date (2022,2,19),
         labels: ["game", "keyboard events"],
     },
     {
         id: "minesSweeper",
         name: "minesSweeper",
         title: "Mines Sweeper!",
-        desc: "Choose your level and try not to explode!, try to beat the High score! ", url: "projs/sokoban",
-        publishedAt: new Date (2022,2,20).getTime(),
+        desc: "Choose your level and try not to explode!, try to beat the High score! ", url: "https://eliyhaoo.github.io/minesSweeper/",
+        publishedAt: new Date (2022,2,20),
         labels: ["game", "keyboard events"],
     },
     {
@@ -45,7 +45,7 @@ var gProjects = [
         name: "touchNums",
         title: "Touch The Numbers",
         desc: "In this project you touch the numbers", url: "projs/sokoban",
-        publishedAt: new Date (2022,2,17).getTime(),
+        publishedAt: new Date (2022,2,17),
         labels: ["game", "keyboard events"],
     },
     {
@@ -53,7 +53,7 @@ var gProjects = [
         name: "touchNums",
         title: "Touch The Numbers",
         desc: "In this project you touch the numbers", url: "projs/sokoban",
-        publishedAt: new Date (2022,2,17).getTime(),
+        publishedAt: new Date (2022,2,17),
         labels: ["game", "keyboard events"],
     },
 ]
