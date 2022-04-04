b21e03242247b0b08ddd2d4a96e3e42f93eb6d99
Eli T <eliyhaoo@gmail.com>
2022-04-04 16:03:07 +0300

    added ME section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
 console.log('Starting up');
+
+function init() {
+    renderProjects()
+}
+
+function renderPrjoects() {
+
+
+}
+
+var gProjects = [
+    {
+        id: "touchNums",
+        name: "touchNums",
+        title: "Touch The Numbers",
+        desc: "In this project you touch the numbers", url: "projs/sokoban",
+        publishedAt: 1448693940000,
+        labels: ["Matrixes", "keyboard events"],
+    }
+]
+
+
+var newDate = new Date (2022,02,17).getTime()
+// var newDatet = newDate.getTime()
+
+console.log(new Date(newDate));
+// console.log('read',newDatet);
